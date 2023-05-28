ef5643708ea5be873d0ed3dd9703a5d2fda6ef6d
Aryan Shafa Wardana <aryan.s.wardana@gmail.com>
2023-05-28 20:05:13 +0700

    feat: add copyable moves

--- Assets/Scripts/FinishedMenu.cs
+++ Assets/Scripts/FinishedMenu.cs
@@ -1,10 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FinishedMenu : MonoBehaviour
 {
+  public TextMeshProUGUI generationsField;
+  public TextMeshProUGUI populationField;
+  public TextMeshProUGUI movesField;
+  public TextMeshProUGUI bestFitnessField;
+
+  private void Awake() {
+    generationsField.text = GameManager.Instance.geneticAlgorithm.currentGeneration.ToString();
+    populationField.text = GameManager.Instance.geneticAlgorithm.populationSize.ToString();
+    movesField.text = GameManager.Instance.geneticAlgorithm.moveSavedCount.ToString();
+    bestFitnessField.text = GameManager.Instance.geneticAlgorithm.bestFitness.ToString();
+  }
+
   public void RunBestFitness() {
     GameManager.Instance.LoadLevelFinished();
+  }
+
+  public void copyMovesToClipboard() {
+    GUIUtility.systemCopyBuffer = string.Join(", ", GameManager.Instance.geneticAlgorithm.moveSaved);
   }
 }
